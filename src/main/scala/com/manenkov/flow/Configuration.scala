cfb441339b4d73c40d8ad0b0fb27b811cb000f55
Valeriy Manenkov <valeriy@manenkov.com>
2022-04-27 12:19:19 +0300

    prepare limits

--- src/main/scala/com/manenkov/flow/Configuration.scala
+++ src/main/scala/com/manenkov/flow/Configuration.scala
@@ -4,4 +4,11 @@ case class Configuration(
                           dailyLimit: Option[Long] = None,
                           weeklyLimit: Option[Long] = None,
                           monthlyLimit: Option[Long] = None,
+                          mondayLimit: Option[Long] = None,
+                          tuesdayLimit: Option[Long] = None,
+                          wednesdayLimit: Option[Long] = None,
+                          thursdayLimit: Option[Long] = None,
+                          fridayLimit: Option[Long] = None,
+                          saturdayLimit: Option[Long] = None,
+                          sundayLimit: Option[Long] = None,
                         )
--- src/main/scala/com/manenkov/flow/Flow.scala
+++ src/main/scala/com/manenkov/flow/Flow.scala
@@ -1,8 +1,23 @@
 package com.manenkov.flow
 
+import java.time.{DayOfWeek, LocalDate}
+
 class Flow(val config: Configuration) {
   def flow(tasks: Seq[Task]): Seq[Task] = {
     tasks.sortBy(_.due)
+  }
+
+  private def dayLimit(date: LocalDate): Long = {
+    val optWeekdayLimit = date.getDayOfWeek match {
+      case DayOfWeek.MONDAY => config.mondayLimit
+      case DayOfWeek.TUESDAY => config.tuesdayLimit
+      case DayOfWeek.WEDNESDAY => config.wednesdayLimit
+      case DayOfWeek.THURSDAY => config.thursdayLimit
+      case DayOfWeek.FRIDAY => config.fridayLimit
+      case DayOfWeek.SATURDAY => config.saturdayLimit
+      case DayOfWeek.SUNDAY => config.sundayLimit
+    }
+    config.dailyLimit.getOrElse(optWeekdayLimit.getOrElse(Long.MaxValue))
   }
 }
 
