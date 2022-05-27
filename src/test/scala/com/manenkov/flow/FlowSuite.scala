84f97f6db8d24c93718f45f275948a9f0e5ba0ad
Valeriy Manenkov <valeriy@manenkov.com>
2022-05-27 12:53:04 +0300

    unit tests

--- src/test/scala/com/manenkov/flow/FlowSuite.scala
+++ src/test/scala/com/manenkov/flow/FlowSuite.scala
@@ -5,11 +5,43 @@ import org.scalatest.funsuite.AnyFunSuite
 import java.time.{LocalDate, LocalDateTime}
 
 class FlowSuite extends AnyFunSuite {
-  test("Empty test") {
-    assert(2 * 2 == 4)
+  test("Per day") {
+    object PerDay {
+      val limit = 2
+      def next: LocalDate => LocalDate = _.plusDays(1)
+      def keyF: T => LocalDate = _.due.toLocalDate
+      def updF: (T, LocalDate) => T = (t, key) => t.copy(due = key.atTime(t.due.toLocalTime))
+    }
+    val original = Seq(
+      T("CA1", isPin = true, LocalDateTime.of(2022, 1, 1, 0, 1)),
+      T("CA2", isPin = true, LocalDateTime.of(2022, 1, 1, 1, 1)),
+      T("CA3", isPin = true, LocalDateTime.of(2022, 1, 1, 2, 1)),
+      T("CB1", isPin = false, LocalDateTime.of(2022, 1, 2, 0, 1)),
+      T("CB2", isPin = false, LocalDateTime.of(2022, 1, 2, 1, 1)),
+      T("CB3", isPin = true, LocalDateTime.of(2022, 1, 2, 2, 1)),
+      T("CC1", isPin = false, LocalDateTime.of(2022, 1, 3, 0, 1)),
+      T("CC2", isPin = false, LocalDateTime.of(2022, 1, 3, 1, 1)),
+      T("CC3", isPin = false, LocalDateTime.of(2022, 1, 3, 2, 1)),
+      T("CC4", isPin = false, LocalDateTime.of(2022, 3, 3, 2, 1)),
+    )
+    val actual = Flow.flow(original, PerDay.limit)(PerDay.keyF, PerDay.next, PerDay.updF)
+    val expected = Seq(
+      T("CA1",isPin = true, LocalDateTime.parse("2022-01-01T00:01")),
+      T("CA2",isPin = true, LocalDateTime.parse("2022-01-01T01:01")),
+      T("CA3",isPin = true, LocalDateTime.parse("2022-01-01T02:01")),
+      T("CB1",isPin = false, LocalDateTime.parse("2022-01-02T00:01")),
+      T("CB3",isPin = true, LocalDateTime.parse("2022-01-02T02:01")),
+      T("CB2",isPin = false, LocalDateTime.parse("2022-01-03T01:01")),
+      T("CC1",isPin = false, LocalDateTime.parse("2022-01-03T00:01")),
+      T("CC2",isPin = false, LocalDateTime.parse("2022-03-03T01:01")),
+      T("CC3",isPin = false, LocalDateTime.parse("2022-03-03T02:01")),
+      T("CC4",isPin = false, LocalDateTime.parse("2022-03-04T02:01")),
+    )
+    assertResult(expected.length)(actual.length)
+    expected.zip(actual).foreach(res => assertResult(res._1)(res._2))
   }
 
-  test("Test2") {
+  test("Per month then per day") {
     val c = Seq(
       T("CA1", isPin = true, LocalDateTime.of(2022, 1, 1, 0, 1)),
       T("CA2", isPin = true, LocalDateTime.of(2022, 1, 1, 1, 1)),
@@ -32,7 +64,7 @@ class FlowSuite extends AnyFunSuite {
 
     object PerMonth {
       val limit = 4
-      def keyF: T => ((Int, Int)) =
+      def keyF: T => (Int, Int) =
         t => (t.due.getYear, t.due.getMonth.getValue)
       def next: ((Int, Int)) => (Int, Int) =
         ym => {
@@ -43,7 +75,22 @@ class FlowSuite extends AnyFunSuite {
         (t, ym) => t.copy(due = t.due.withYear(ym._1).withMonth(ym._2))
     }
 
-    val stage1 = Flow.flow(c, PerDay.limit)(PerDay.keyF, PerDay.next, PerDay.updF)
-    val stage2 = Flow.flow(stage1, PerMonth.limit)(PerMonth.keyF, PerMonth.next, PerMonth.updF)
+    val stage1 = Flow.flow(c, PerMonth.limit)(PerMonth.keyF, PerMonth.next, PerMonth.updF)
+    val stage2 = Flow.flow(stage1, PerDay.limit)(PerDay.keyF, PerDay.next, PerDay.updF)
+
+    val expected = Seq(
+      T("CA1", isPin = true, LocalDateTime.parse("2022-01-01T00:01")),
+      T("CA2", isPin = true, LocalDateTime.parse("2022-01-01T01:01")),
+      T("CA3", isPin = true, LocalDateTime.parse("2022-01-01T02:01")),
+      T("CB3", isPin = true, LocalDateTime.parse("2022-01-02T02:01")),
+      T("CB1", isPin = false, LocalDateTime.parse("2022-03-02T00:01")),
+      T("CB2", isPin = false, LocalDateTime.parse("2022-03-02T01:01")),
+      T("CC1", isPin = false, LocalDateTime.parse("2022-03-03T00:01")),
+      T("CC2", isPin = false, LocalDateTime.parse("2022-03-03T01:01")),
+      T("CC3", isPin = false, LocalDateTime.parse("2022-04-03T02:01")),
+      T("CC4", isPin = false, LocalDateTime.parse("2022-04-03T02:01")),
+    )
+    assertResult(expected.length)(stage2.length)
+    expected.zip(stage2).foreach(pair => assertResult(pair._1)(pair._2))
   }
 }
